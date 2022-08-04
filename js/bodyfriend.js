bfe9c733ab40a53ed544f926f3786e5999df7588
chxxing <choing26@naver.com>
2022-08-04 17:50:11 +0900

    미완성

---
+++ js/bodyfriend.js
@@ -0,0 +1,42 @@
+$(function () {
+    $('#bgndVideo').YTPlayer({
+        videoURL: 'https://youtu.be/W0eiqqAdaeA?list=PLvjr4SLUAy2UTjzXs2PltS7KZeVkuD3qw',
+        containment: '.main_video',
+        autoPlay: true,
+        mute: true,
+        playOnlyIfVisible: true,
+        loop: true,
+        abundance: 0.12,
+    });
+
+    // $('a').on('click', function (e) {
+    //     e.preventDefault();
+    //     // console.log($(this.hash).offset().top);
+    //     var hash = $(this.hash).offset().top;
+    //     $('html,body').animate({ scrollTop: hash }, 500)
+    // });
+
+    $('.tab_menu>li>a').on('click', function (e) {
+        e.preventDefault();
+        // console.log($(this).parent().index());
+        var idx = $(this).parent().index();
+        $('.tab_content>li').removeClass('on');
+        $('.tab_content>li').eq(idx).addClass('on');
+        $('.tab_menu>li').removeClass('on');
+        $(this).parent().addClass('on');
+    });
+
+    $('.shop_slider').slick({
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 3,
+    });
+
+    $('.shop_arrow i:nth-child(1)').on('click', function () {
+        $('.shop_slider').slick('slickPrev');
+    })
+
+    $('.shop_arrow i:nth-child(2)').on('click', function () {
+        $('.shop_slider').slick('slickNext');
+    })
+})
